862fa442e3d0870832ef8749bc9a108a7288d8fa
Madison <madison.brock03@gmail.com>
2022-02-18 13:46:34 -0600

    Literal Math Exercise

---
+++ script.js
@@ -0,0 +1,17 @@
+// EXERCISE
+
+let warmHugs= "Hi, I\'m Olaf and I like warm hugs."
+console.log(warmHugs.toUpperCase());// HI, I'M OLAF AND I LIKE WARM HUGS.
+console.log(warmHugs.replace("like", "love"));// Hi, I'm Olaf and I love warm hugs.
+
+let beenImpaled= "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18, 36));// I've been impaled.
+
+let dotDotDot= "..."
+let skullBones= `I don't have a skull ${dotDotDot} or bones.`
+console.log(skullBones);// I don't have a skull ... or bones.
+
+console.log(Math.PI);// 3.141592653589793
+
+let randomNum = Math.random (1, 3)
+console.log(randomNum);// 0.17821601714498536
